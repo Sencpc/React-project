d48affabc89c51446b98894f1e70d43b644a21a4
Sen <seancornelius11@gmail.com>
2025-12-19 20:51:37 +0700

    fix login

--- Frontend/src/config/env.js
+++ Frontend/src/config/env.js
@@ -1,7 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL;
-
-if (!API_BASE_URL) {
-  throw new Error("VITE_API_URL is not defined.");
-}
+const API_BASE_URL = import.meta.env.VITE_API_URL || "";
 
 export { API_BASE_URL };
