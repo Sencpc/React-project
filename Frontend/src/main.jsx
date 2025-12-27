fa20c218cf4d76802d4072547a07832209b7821a
Nicks911 <nathanaelnicholas99@gmail.com>
2025-12-27 11:59:55 +0700

    implement ant design home,regis,login

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import "antd/dist/reset.css";
 import "./index.css";
 import App from "./App.jsx";
 import store from "./store/index.js";
